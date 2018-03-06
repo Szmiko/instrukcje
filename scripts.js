2daba4816e2a74035b96157d1d5686a5c2a0ad95
Szymon Grabowski <szmiko@wp.pl>
2018-03-06 19:54:47 +0100

    Add operation

---
+++ scripts.js
@@ -0,0 +1,13 @@
+var a = prompt ('Podaj cyfrę (a)'),
+	b = prompt ('Podaj cyfrę (b)'),
+	value = (a * a) - (2 * a * b) - (b * b);
+
+console.log(value);
+
+if (value > 0) {
+	console.log('Wynik dodatni')
+} else if (value < 0) {
+	console.log('Wynik ujemny')
+} else if (value = 0) {
+	console.log('Wynik jest równy 0')
+}
